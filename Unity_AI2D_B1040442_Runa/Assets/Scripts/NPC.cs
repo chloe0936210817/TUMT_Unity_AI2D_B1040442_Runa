15f5aa04281494a2f3e00502a125a4e15e334ee6
chloe0936210817 <55518289+chloe0936210817@users.noreply.github.com>
2020-01-07 16:46:27 +0800

    NPC

---
+++ Unity_AI2D_B1040442_Runa/Assets/Scripts/NPC.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+public string SayStart="你好啊，冒險者。";
+    public string SayNotComplete = "不不不，我丟的金幣可沒這麼少。";
+    public string SayComplete = "你真是偉大的冒險者，太感謝了。";
+
+    public float Speed = 1.5f;
+
+    public bool Complete = false;
+    public 
+
+}
---
+++ Unity_AI2D_B1040442_Runa/Assets/Scripts/Protagonist.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Protagonist : MonoBehaviour
+{
+    public int speed = 50;
+    public float jump = 2.5f;
+    public string protagonistName = "主角";
+    public bool pass = false;
+
+}
