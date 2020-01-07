f1ebfc6030c323568bc40a7b6db01a7f50bcfc0f
chloe0936210817 <55518289+chloe0936210817@users.noreply.github.com>
2020-01-07 17:58:51 +0800

    主角

--- Unity_AI2D_B1040442_Runa/Assets/Scripts/NPC.cs
+++ Unity_AI2D_B1040442_Runa/Assets/Scripts/NPC.cs
@@ -1,14 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class NPC : MonoBehaviour
-{
-public string SayStart="你好啊，冒險者。";
-    public string SayNotComplete = "不不不，我丟的金幣可沒這麼少。";
-    public string SayComplete = "你真是偉大的冒險者，太感謝了。";
+public class NPC : MonoBehaviour {
 
-    public float Speed = 1.5f;
-
-    public bool Complete = false;
-    public 
-
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
--- Unity_AI2D_B1040442_Runa/Assets/Scripts/Protagonist.cs
+++ Unity_AI2D_B1040442_Runa/Assets/Scripts/Protagonist.cs
@@ -7,4 +7,25 @@ public class Protagonist : MonoBehaviour
     public string protagonistName = "主角";
     public bool pass = false;
 
+    private Rigidbody2D r2d;
+    private Transform tra;
+
+    //開始事件
+    private void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+        tra = GetComponent<Transform>();
+    }
+
+    //更新事件
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) tra.eulerAngles = new Vector3(0, 0, 0);
+    }
+
+    //固定更新事件
+    private void FixedUpdate()
+    {
+        r2d . AddForce(new Vector2(speed*Input.GetAxis("Horizontal"), 0));
+    }
 }
