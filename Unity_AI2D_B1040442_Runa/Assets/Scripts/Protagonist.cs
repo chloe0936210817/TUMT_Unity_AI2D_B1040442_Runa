c6e220e0e97bdb3e7544d5a5f699fd830fe347d7
chloe0936210817 <55518289+chloe0936210817@users.noreply.github.com>
2020-01-07 22:40:25 +0800

    跳躍

--- Unity_AI2D_B1040442_Runa/Assets/Scripts/Protagonist.cs
+++ Unity_AI2D_B1040442_Runa/Assets/Scripts/Protagonist.cs
@@ -8,24 +8,56 @@ public class Protagonist : MonoBehaviour
     public bool pass = false;
 
     private Rigidbody2D r2d;
-    private Transform tra;
+    //private Transform tra;
 
     //開始事件
     private void Start()
     {
         r2d = GetComponent<Rigidbody2D>();
-        tra = GetComponent<Transform>();
+        //tra = GetComponent<Transform>();
     }
 
     //更新事件
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D)) tra.eulerAngles = new Vector3(0, 0, 0);
+        if (Input.GetKeyDown(KeyCode.D)) Turn(0);
+        if (Input.GetKeyDown(KeyCode.A)) Turn(180);
     }
 
     //固定更新事件
     private void FixedUpdate()
     {
+        Walk();
+        Jump();
+    }
+
+    //走路
+    private void Walk()
+    {
         r2d . AddForce(new Vector2(speed*Input.GetAxis("Horizontal"), 0));
     }
+
+    //跳躍
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        r2d.AddForce(new Vector2(0,jump));
+    }
+
+    //右轉
+    private void TurnRight()
+    {
+        transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+    private void TurnLeft()
+    {
+        transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+
+    //轉彎
+    private void Turn(int direction)
+    {
+        transform.eulerAngles = new Vector3(0, direction, 0);
+    }
+
 }
